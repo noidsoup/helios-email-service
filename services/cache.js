703af2a0fd059d29819ce135756a5240ee559083
noidsoup <nicholas.eymann@gmail.com>
2019-02-10 20:49:28 -0800

    wat

--- services/cache.js
+++ services/cache.js
@@ -22,11 +22,11 @@ const redis_host = process.env.REDIS_CLIENT_URL || 'localhost';
 
 console.log('-----------process.env.REDIS_CLIENT_URL---------------', redis_host);
 
-const testFolder = '../../';
+const testFolder = '../';
 const fs = require('fs');
 
 fs.readdirSync(testFolder).forEach(file => {
-  console.log('69 ---------------------', file);
+  console.log('70 ---------------------', file);
 })
 
 const client = redis.createClient({ host: redis_host });
