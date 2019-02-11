2ad7d9e80f00fc0f358908c3b6382ae475c43819
nicholas eymann <nicholas.eymann@heliosinteractive.com>
2019-02-11 12:53:43 -0800

    wat

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ const testFolder = './';
 const fs = require('fs');
 
 fs.readdirSync(testFolder).forEach(file => {
-  console.log('7 ---------------------', file);
+  console.log('8 ---------------------', file);
 })
 
 app.use(router);
